268650da6799174fdca41cddd5809616af333a05
Piro Lera <pirolera@gmail.com>
2019-04-24 23:38:54 -0400

    adding script to build JSON package

---
+++ buildInstallJSONDep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d json ]
+then
+  git clone https://github.com/nlohmann/json.git
+fi
+
+cd json/
+if [ ! -d build]
+then
+  mkdir build
+fi
+
+cd build/
+cmake ../
+make -j install
